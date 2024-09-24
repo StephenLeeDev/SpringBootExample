e62720b77624dca7345ecdadddc3a1ce2f789b1e
StephenLeeDev <stephen.lee.dev@gmail.com>
2024-09-24 21:59:14 +0900

    User page - Register

---
+++ src/main/java/com/stephenleedev/SpringBoot/Example/controller/MemberForm.java
@@ -0,0 +1,15 @@
+package com.stephenleedev.SpringBoot.Example.controller;
+
+public class MemberForm {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
