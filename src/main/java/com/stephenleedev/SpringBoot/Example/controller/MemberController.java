66240aacf397cfcdfe68e4b39499ffd269af6123
StephenLeeDev <stephen.lee.dev@gmail.com>
2024-09-29 14:47:49 +0900

    User page - Get users

--- src/main/java/com/stephenleedev/SpringBoot/Example/controller/MemberController.java
+++ src/main/java/com/stephenleedev/SpringBoot/Example/controller/MemberController.java
@@ -4,8 +4,11 @@ import com.stephenleedev.SpringBoot.Example.domain.Member;
 import com.stephenleedev.SpringBoot.Example.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -33,4 +36,10 @@ public class MemberController {
         return "redirect:/";
     }
 
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
 }
