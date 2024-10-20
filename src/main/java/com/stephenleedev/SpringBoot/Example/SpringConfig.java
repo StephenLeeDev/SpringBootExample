6b55611fa42d8b1d3c4ba2459a5ca543b3bb25ac
StephenLeeDev <stephen.lee.dev@gmail.com>
2024-10-20 16:48:17 +0900

    JdbcTemplate

--- src/main/java/com/stephenleedev/SpringBoot/Example/SpringConfig.java
+++ src/main/java/com/stephenleedev/SpringBoot/Example/SpringConfig.java
@@ -1,7 +1,9 @@
 package com.stephenleedev.SpringBoot.Example;
 
 import com.stephenleedev.SpringBoot.Example.repository.JdbcMemberRepository;
+import com.stephenleedev.SpringBoot.Example.repository.JdbcTemplateMemberRepository;
 import com.stephenleedev.SpringBoot.Example.repository.MemberRepository;
+import com.stephenleedev.SpringBoot.Example.repository.MemoryMemberRepository;
 import com.stephenleedev.SpringBoot.Example.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -24,6 +26,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new JdbcMemberRepository(dataSource);
+//        return new MemoryMemberRepository();
+//        return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
