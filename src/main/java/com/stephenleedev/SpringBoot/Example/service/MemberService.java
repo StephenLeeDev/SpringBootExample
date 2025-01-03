3e52163fe1ec505d3069e1db406543d933f3e8e0
StephenLeeDev <stephen.lee.dev@gmail.com>
2025-01-03 21:29:28 +0900

    AOP

--- src/main/java/com/stephenleedev/SpringBoot/Example/service/MemberService.java
+++ src/main/java/com/stephenleedev/SpringBoot/Example/service/MemberService.java
@@ -20,11 +20,19 @@ public class MemberService {
      * 회원가입
      */
     public Long join(Member member) {
-        // 중복 회원 검증
-        validateDuplicateMember(member);
+        long start = System.currentTimeMillis();
 
-        repository.save(member);
-        return member.getId();
+        try {
+            // 중복 회원 검증
+            validateDuplicateMember(member);
+
+            repository.save(member);
+            return member.getId();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join = " + timeMs + "ms");
+        }
     }
 
     private void validateDuplicateMember(Member member) {
@@ -38,7 +46,15 @@ public class MemberService {
      * 전체 회원 조회
      */
     public List<Member> findMembers() {
-        return repository.findAll();
+        long start = System.currentTimeMillis();
+
+        try {
+            return repository.findAll();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("findMembers = " + timeMs + "ms");
+        }
     }
 
     /**
